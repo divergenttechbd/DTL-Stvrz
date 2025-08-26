c9bcfcb45690a349113b895604c7f5548cc76c9d
tibrow <dev.tibrow@gmail.com>
2025-08-26 15:54:14 +0600

    redis

--- src/myproject/settings/third_party.py
+++ src/myproject/settings/third_party.py
@@ -85,7 +85,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("143.110.191.153", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
