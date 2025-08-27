996dd66b8e23fc3db7846112ac5d7b77375c75b1
tibrow <dev.tibrow@gmail.com>
2025-08-27 18:02:36 +0600

    TOKEN

--- src/myproject/settings/apps.py
+++ src/myproject/settings/apps.py
@@ -55,21 +55,9 @@ DEFAULT_MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://dev.stayverz.com",
-    "https://dev-admin.stayverz.com",
-    "https://local.stayverz.com",
-    "https://dev.bdbnb.com",
-    "https://dev-admin.bdbnb.com",
-    "https://redesign.stayverz.com",
-    "https://www.stayverz.com",
-    "https://stayverz.com",
-    "https://admin.stayverz.com",
-    "https://new.stayverz.com",
-]
 
 ON_TOP_MIDDLEWARE = [
-     "corsheaders.middleware.CorsMiddleware",
+     # "corsheaders.middleware.CorsMiddleware",
 ]
 
 THIRD_PARTY_MIDDLEWARE = []
