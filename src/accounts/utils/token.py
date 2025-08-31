6591540c851b2adf49ea415b9cafea4203df3b1b
tibrow <dev.tibrow@gmail.com>
2025-08-31 16:10:15 +0600

    token

--- src/accounts/utils/token.py
+++ src/accounts/utils/token.py
@@ -43,7 +43,7 @@ def create_tokens(user: User) -> tuple[str, str]:
 
 
 def generate_cookie_data(access_token: str) -> dict:
-    domain = "stayverz.divergenttechbd.com" if settings.ENVIRONMENT == "dev" else "stayverz.divergenttechbd.com"
+    domain = "stayverz.com" if settings.ENVIRONMENT == "dev" else "stayverz.com"
 
     return {
         "key": "cookie_token",
